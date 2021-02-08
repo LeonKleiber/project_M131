6633d61f61628d56ba07e94afe8542957fe7ea20
kleiber <kleiber@adobe.com>
2021-02-08 06:26:53 +0100

    Update Dropdown.js

--- timetable/src/Components/Dropdown.js
+++ timetable/src/Components/Dropdown.js
@@ -1,20 +1,20 @@
-import Alert from "./Alert";
+import Alert from './Alert';
 
 const Dropdown = ({ title, options, value, onChange }) => {
 	if (options.length)
 		return (
-			<div className="form-floating mb-5">
-				<select className="form-select" value={value} onChange={(e) => onChange(e.target.value)} id={title}>
-					<option value="0" disabled>Select {title}</option>
+			<div className='form-floating mb-5'>
+				<select className='form-select' value={value} onChange={(e) => onChange(e.target.value)} id={title}>
+					<option value='0' disabled>Select {title}</option>
 					{options.map(option => <option value={option.value} key={option.value}>{option.name}</option>)}
 				</select>
-				<label htmlFor={title} className="text-dark">{title}</label>
+				<label htmlFor={title} className='text-dark'>{title}</label>
 			</div>
 		)
 	else
 		return (
-			<div className="mb-5">
-				<Alert text="Something went wrong" type="danger" />
+			<div className='mb-5'>
+				<Alert text={`Couldn't load ${title}`} type='danger' />
 			</div>
 		)
 }
